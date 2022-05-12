bfce4dc8f9732b4be75c78769378e69c2c11cc3a
jennigoberski <jenni.goberski@gmail.com>
2022-05-12 17:17:43 -0300

    Adding heritage exercise

---
+++ javascript.curso/src/secao6aula95/heranca.js
@@ -0,0 +1,57 @@
+//Produto -> aumento, deconto
+//Camiseta= cor, caneca = material
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.aumento = function(quantia) {
+    this.preco += quantia;
+};
+
+Produto.prototype.desconto = function(quantia) {
+    this.preco -= quantia;
+};
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+}
+
+//Criando um objeto vazio e setando seu prototype igual ao prototype de Produto
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+Camiseta.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco *(percentual / 100));
+};
+
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+    
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+            return estoque;
+        },
+        set: function(valor) {
+            if (tipeof (valor) !== 'number') return;
+            estoque = valor;
+        }
+    });
+}
+
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+const produto = new Produto('Gen', 10);
+const camiseta = new Camiseta('Regata',7.5, 'Preta');
+const caneca = new Caneca('Caneca', 13, 'Plástico', 5);
+caneca.estoque = 100;
+
+console.log(produto);
+console.log(camiseta);
+console.log(caneca);
+
+//Estamos, claramente, falando que o Camiseta.prototype é um novo objeto vazio que tem como protótipo Produto.prototype. Dessa forma, estamos adicionando Camiseta.prototype na cadeia de protótipos de Produto. Por consequência, tudo o que não existir em Camiseta nem em Camiseta.prototype será delegado para Produto.prototype (e assim por diante até chegar no topo da cadeia de protótipos). Por isso vejo o termo delegação como mais adequado para este comportamento.
